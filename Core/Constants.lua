9339ffedbb6bc84d66cdaf1e342f93df7033d9ba
Antonio Lagnada <antonio@lagnada.com>
2022-01-02 21:40:31 -0800

    settings wireframe

--- Core/Constants.lua
+++ Core/Constants.lua
@@ -1,8 +1,7 @@
 if type(ABP_LOG_LEVEL) ~= "number" then ABP_LOG_LEVEL = 1 end
 if type(ABP_DEBUG_MODE) ~= "boolean" then ABP_DEBUG_MODE = false end
 
-ABP_PREFIX_ADDON = '{{|cfd2db9fbActionBar|r}} '
-ABP_PREFIX = '{{|cfd2db9fbActionBar|r|cfdfbeb2dPlus::%s|r}} '
+ABP_PREFIX = '{{|cfd2db9fbActionBarPlus|r|cfdfbeb2d::%s|r}} '
 local settings = LibStub:GetLibrary("ActionbarPlus-Settings-1.0")
 local buttonsUI = LibStub:GetLibrary("ActionbarPlus-ButtonUI-1.0")
 
