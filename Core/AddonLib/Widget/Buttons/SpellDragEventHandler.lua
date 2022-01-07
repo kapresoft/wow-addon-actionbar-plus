6d802a35e4e8114bb1ea53e572fe69515b7fcc1b
Antonio Lagnada <antonio@lagnada.com>
2022-01-07 14:36:48 -0800

    remove logs

--- Core/AddonLib/Widget/Buttons/SpellDragEventHandler.lua
+++ Core/AddonLib/Widget/Buttons/SpellDragEventHandler.lua
@@ -13,25 +13,19 @@ SpellDragEventHandler = S
 function S:Handle(btnUI, spellCursorInfo)
     if spellCursorInfo == nil or spellCursorInfo.id == nil then return end
     local spellInfo = _API_Spell:GetSpellInfo(spellCursorInfo.id)
-    AssertNotNil(spellInfo, 'spellInfo')
+    if Assert.IsNil(spellInfo) then return end
     --self:logp('spellInfo', spellInfo)
 
     local actionbarInfo = btnUI:GetActionbarInfo()
-    self:logp('ActionBar', actionbarInfo)
+    --self:logp('ActionBar', actionbarInfo)
     local btnName = btnUI:GetName()
     local barData = P:GetBar(actionbarInfo.index)
-    --local key = actionbarInfo.name .. btnName
-    --P.profile[key] = P:GetTemplate().Button
-    --local btnData = P.profile[key]
-    --self:logp({ actionbar=actionbarInfo.name, frameIndex=actionbarInfo.index, value=barData })
 
     local btnData = barData.buttons[btnName] or P:GetTemplate().Button
     btnData.type = ButtonAttributes.SPELL
     btnData[ButtonAttributes.SPELL] = spellInfo
     barData.buttons[btnName] = btnData
-    --P.profile.bars[actionbarInfo.name].buttons[btnName] = btnData
-    --P.profile.bars[actionbarInfo.name] = barData
-    self:logp('Saved', { actionbar=frameName, btn={ name=btnName, value=btnData }})
+    --self:logp('Saved', { actionbar=frameName, btn={ name=btnName, value=btnData }})
 
     SpellAttributeSetter(btnUI, btnData)
 end
