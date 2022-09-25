e01f8331d4283c1098069b2d8801ec7bc65c1349
Tony Lagnada <deloyz@gmail.com>
2022-09-25 14:06:38 -0700

    Use common release dir in packager

--- sync-extlibs.sh
+++ sync-extlibs.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-# Usage
-#   ./sync-extlibs.sh
-# Overwrite .release dir
-#   ./sync-extlibs.sh -o
+# Use Common Release Dir
+RELEASE_DIR=~/.release
+ADDON_NAME="ActionbarPlus"
 
 Package() {
   local arg1=$1
-  local rel_dir=./.release
-  local rel_cmd="release-wow-addon -cdzul"
+  local rel_dir=$RELEASE_DIR
+  local rel_cmd="release-wow-addon -r ${RELEASE_DIR} -cdzul"
 
   if [[ "$arg1" == "-h" ]]; then
     echo "Usage: $0 [-o]"
     echo "Options:  "
-    echo "  -o to keep existing ./.release directory"
+    echo "  -o to keep existing release directory"
     exit 0
   fi
 
-  if [[ -d ./.release ]]; then
+  if [[ -d ${RELEASE_DIR} ]]; then
     echo "$rel_dir dir exists"
     if [[ "$arg1" == "-o" ]]; then
       rel_cmd="${rel_cmd}o"
@@ -28,8 +27,7 @@ Package() {
 }
 
 SyncExtLib() {
-
-  local cmd='rsync -aucv --progress --inplace --out-format="[Modified: %M] %o %n%L" ".release/ActionbarPlus/Core/ExtLib/WoWAce/" "Core/ExtLib/WoWAce/"'
+  local cmd='rsync -aucv --progress --inplace --out-format="[Modified: %M] %o %n%L" "${RELEASE_DIR}/${ADDON_NAME}/Core/ExtLib/WoWAce/" "Core/ExtLib/WoWAce/"'
   echo "Executing: $cmd"
   eval "$cmd"
 }
