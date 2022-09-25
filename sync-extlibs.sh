1fb1f0d38414468c7020d5268cc0a1ce321a27bd
Tony Lagnada <deloyz@gmail.com>
2022-09-25 14:15:00 -0700

    pass through all packager options

--- sync-extlibs.sh
+++ sync-extlibs.sh
@@ -7,7 +7,7 @@ ADDON_NAME="ActionbarPlus"
 Package() {
   local arg1=$1
   local rel_dir=$RELEASE_DIR
-  local rel_cmd="release-wow-addon -r ${RELEASE_DIR} -cdzul"
+  local rel_cmd="release-wow-addon -r ${RELEASE_DIR} -cdzul $*"
 
   if [[ "$arg1" == "-h" ]]; then
     echo "Usage: $0 [-o]"
@@ -18,9 +18,7 @@ Package() {
 
   if [[ -d ${RELEASE_DIR} ]]; then
     echo "$rel_dir dir exists"
-    if [[ "$arg1" == "-o" ]]; then
-      rel_cmd="${rel_cmd}o"
-    fi
+    rel_cmd="${rel_cmd}"
   fi
   echo "Executing: $rel_cmd"
   eval "$rel_cmd"
