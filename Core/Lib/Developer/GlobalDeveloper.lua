1303806a9a0487621aaa3059fb4617e37c83cf11
Tony Lagnada <deloyz@gmail.com>
2024-04-09 15:36:46 -0700

    Resolves #392: Hide Debugging Tab for non debug builds

--- Core/Lib/Developer/GlobalDeveloper.lua
+++ Core/Lib/Developer/GlobalDeveloper.lua
@@ -1,16 +1,22 @@
 --- @type string
 local addon
 --- @type Namespace | Kapresoft_Base_Namespace
-local ns
-addon, ns = ...
+local kns
+addon, kns = ...
 local sformat = string.format
-local c1 = PURE_RED_COLOR
+local c1 = CreateColor(0.9, 0.2, 0.2, 1.0)
 local c2 = WHITE_FONT_COLOR
 
-local prefix = sformat('{{%s::%s}}::', c1:WrapTextInColorCode(addon), c2:WrapTextInColorCode('GlobalDeveloper'))
+local prefix = sformat('{{%s::%s}}:', c1:WrapTextInColorCode(addon), c2:WrapTextInColorCode('GlobalDeveloper'))
+
+local function log(...) print(prefix, ...) end
+
 --[[-----------------------------------------------------------------------------
 Main Code
 -------------------------------------------------------------------------------]]
+local flag = kns.debug.flag
+flag.debugging = true
+flag.eventTrace = true
 
-ns.enableEventTrace = true
-print(prefix, 'ns.enableEventTrace:', ns.enableEventTrace)
+log('debug.flag.debugging:', flag.debugging)
+log('debug.flag.eventTrace:', flag.eventTrace)
