8c3a81b1c0e5de17bb15092afe24cf31f230e126
Tony Lagnada <deloyz@gmail.com>
2024-04-19 17:19:17 -0700

    Enable debug level quick buttons in debugging tab.

--- Core/Lib/Locales/enUS.lua
+++ Core/Lib/Locales/enUS.lua
@@ -83,6 +83,7 @@ L['Debugging::Category::Disable All::Button']            = 'Disable All'
 L['Debugging::Category::Disable All::Button::Desc']      = 'Disables all log categories below. Note that the default category (not shown here) will always be active.'
 L['Log Level']                                           = true
 L['Log Level::Description']                              = 'Higher log levels generate more logs:\nLog Levels: ERROR(5), WARN(10), INFO(15), DEBUG(20), FINE(25), FINER(30), FINEST(35), TRACE(50)'
+L['Categories'] = true
 
 -- new
 
