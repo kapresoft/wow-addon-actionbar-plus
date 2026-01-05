9f3fedf7a17994676dce54b6feb0612de2ffaa24
Tony Lagnada <deloyz@gmail.com>
2026-01-04 16:16:21 -0800

    pull-ext-lib updates, others.

--- dev/pull-extlib.sh
+++ dev/pull-extlib.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env zsh
 
-BUILD_DIR=./build
+ADDON_NAME=ActionbarPlus
+
+BUILD_DIR=./.build
 SCRIPT_DIR=./dev
 RELEASE_SCRIPT=${SCRIPT_DIR}/release.sh
+PACKAGE_NAME=Pull-Ext-Lib
+EXT_LIB_DIR=ExtLib
 
 _Release() {
     if [[ "$1" = "" ]]; then
@@ -14,6 +18,21 @@ _Release() {
     local cmd="${RELEASE_SCRIPT} ${args}"
     echo "Executing: ${cmd}"
     eval "${cmd}" && echo "Execution Complete: ${cmd}"
+}
+
+_PostBuild() {
+  echo ">> POST BUILD ACTIONS"
+  local dest1="./${ADDON_NAME}/Core/${EXT_LIB_DIR}"
+  if [[ ! -d "${dest1}" ]]; then
+    echo "Creating dir: ${dest1}"
+    mkdir ${dest1} || {
+      echo "Failed to create dir: ${dest1}"
+      return 0
+    }
+  fi
+  local cmd1="cp -r ./build/${PACKAGE_NAME}/${ADDON_NAME}/Core/${EXT_LIB_DIR}/. ${dest1}"
+    echo "Executing: ${cmd1}"
+    eval "${cmd1}" && echo "Execution Complete: ${cmd1}"
 }
 
 #_Release pkgmeta-kapresoftlibs-interface.yaml
