d2fb211ad74bab1edbd860abe8aa9a3a9defbbb9
Tony Lagnada <deloyz@gmail.com>
2026-02-04 17:15:58 -0800

    Hide Actionbars on ghost form #548

--- Core/Lib/Widget/Controllers/PlayerGhostStateController.lua
+++ Core/Lib/Widget/Controllers/PlayerGhostStateController.lua
@@ -1,3 +1,9 @@
+--[[-------------------------------------------------------------------
+Blizzard Vars
+---------------------------------------------------------------------]]
+local C_Timer = C_Timer
+local UnitIsGhost = UnitIsGhost
+
 --[[-----------------------------------------------------------------------------
 Local Vars
 -------------------------------------------------------------------------------]]
@@ -12,9 +18,6 @@ local libName = 'PlayerGhostStateController'
 --- @class PlayerGhostStateController
 local L = ns:NewController(libName)
 local p = ns:CreateDefaultLogger(libName)
---C_Timer.After(1, function()
---    print('xx loaded...')
---end)
 
 --[[-----------------------------------------------------------------------------
 Methods
@@ -26,11 +29,6 @@ local function PropsAndMethods(o)
   --- @see ModuleV2Mixin#Init
   --- @private
   function o:OnAddOnReady()
-    -- todo: need user settings per bar, hide_when_ghost_form, default: true
-    -- todo: don't check-in yet, we may need a general 'visibility' field
-    -- todo: meant for: Story: Option to Hide Actionbars when Dead #457
-    --       but will be solved by #5 Use State Driver for Visibility State
-
     self:RegisterAddOnMessage(E.PLAYER_ALIVE, o.OnPlayerAlive)
     self:RegisterAddOnMessage(E.PLAYER_UNGHOST, o.OnPlayerUnghost)
   end
@@ -39,17 +37,14 @@ local function PropsAndMethods(o)
   --- or accepts a resurrect before releasing their spirit.
   function o.OnPlayerAlive()
     local isGhost = UnitIsGhost('player')
-    p:vv(function() return 'xxx OnPlayerAlive() unitIsGhost=%s', isGhost end)
     if not isGhost then return end
     C_Timer.After(0.2, function() o:HideAll() end)
   end
 
   --- Fired when the player is alive after being a ghost.
   function o.OnPlayerUnghost()
-    p:vv(function() return 'xxx OnPlayerUnghost() called... end)' end)
     C_Timer.After(0.2, function() o:ShowAll() end)
   end
 
-end;
-PropsAndMethods(L)
+end; PropsAndMethods(L)
 
