9398b5b0df77cecab83e5fb1b03c511a91d7f9fd
Tony Lagnada <deloyz@gmail.com>
2026-02-10 11:47:06 -0800

    ABPBarsUI

--- ActionbarPlus-Core/Libs/ActionbarPlus-Core.lua
+++ ActionbarPlus-Core/Libs/ActionbarPlus-Core.lua
@@ -3,7 +3,7 @@ local ns = select(2, ...)
 
 local p, t1, t2 = ns:log()
 
-C_Timer.After(2, function()
+C_Timer.After(1, function()
   t1('xx loaded....')
   p('xxR loaded...')
   print('xx loaded... tag=', ns.printer)
