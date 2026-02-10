921c9d2e39c7689af59ebd5861bda62a7aebba93
Tony Lagnada <deloyz@gmail.com>
2026-02-10 10:22:07 -0800

    Update setup to build for v2

--- dev/setup.sh
+++ dev/setup.sh
@@ -4,6 +4,7 @@ BUILD_DIR=./.release
 RELEASE_SCRIPT=./dev/release.sh
 TOC_FILE="setup.toc"
 PKGMETA_FILE="setup.yml"
+PKGMETA_FILE_V2="setupV2.yml"
 
 p() {
   printf "%-9s: %-40s\n" "$1" "$2"
@@ -29,20 +30,28 @@ ensure_file() {
 # -r  releasedir    Set directory containing the package directory. Defaults to "$topdir/.release".
 # -m  pkgmeta.yaml  Set the pkgmeta file to use.
 _Release() {
-    local pkgmeta_path="./dev/${PKGMETA_FILE}"
+    local opt=$1
+    local pkgmeta_name="setup"
+
+    local pkgmeta="${PKGMETA_FILE}"
+    if [[ "$opt" == "v2" ]]; then
+      pkgmeta="${PKGMETA_FILE_V2}"
+    fi
+    local pkgmeta_path="./dev/${pkgmeta}"
+    echo "pkgmeta_path=${pkgmeta_path}"
 
     ensure_dir "$BUILD_DIR"
-    cp "${pkgmeta_path}" "_${PKGMETA_FILE}" || {
+    cp "${pkgmeta_path}" "_${pkgmeta_name}" || {
       echo "Missing: ${pkgmeta_path}"
       return 1
     }
-    ensure_file "./_${PKGMETA_FILE}" || {
+    ensure_file "./_${pkgmeta_name}" || {
       p "Missing: $file"
       return 1
     }
     cp ./dev/${TOC_FILE} _${TOC_FILE}
 
-    local args="-duz -r ${BUILD_DIR} -m _${PKGMETA_FILE}"
+    local args="-duz -r ${BUILD_DIR} -m _${pkgmeta_name}"
     local cmd="${RELEASE_SCRIPT} ${args}"
     echo "Executing: ${cmd}"
     (eval "${cmd}" && echo "Execution Complete: ${cmd}") || {
@@ -50,9 +59,9 @@ _Release() {
       return 1
     }
     echo "Cleaning up..." && {
-      rm _${PKGMETA_FILE}
+      rm _${pkgmeta_name}
       rm _${TOC_FILE}
     }
 }
 
-_Release
+_Release $*
