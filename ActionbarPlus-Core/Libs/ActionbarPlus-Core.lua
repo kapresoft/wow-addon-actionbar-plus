dc38e715dbf843dee61dfde811aebc4ce13fc23a
Tony Lagnada <deloyz@gmail.com>
2026-02-10 16:06:49 -0800

    Core Init and OnEnable

--- ActionbarPlus-Core/Libs/ActionbarPlus-Core.lua
+++ ActionbarPlus-Core/Libs/ActionbarPlus-Core.lua
@@ -1,11 +1,22 @@
 --- @type Namespace_ABP_2_0
 local ns = select(2, ...)
-
 local p, t1, t2 = ns:log()
 
-C_Timer.After(1, function()
-  t1('xx loaded....')
-  p('xxR loaded...')
-  print('xx loaded... tag=', ns.printer)
-end)
-XNS = ns
+--- @class ABP_Core : AceAddonObj
+local A = ns.AceAddon:NewAddon(ns.name, "AceEvent-3.0", "AceBucket-3.0", "AceConsole-3.0")
+--ActionbarPlus_Core = ABP_Core
+
+--- @type ABP_Core
+local o = A
+
+-- Called once, after:
+-- • SavedVariables are loaded
+-- • All addon Lua/XML files are loaded
+-- • Init default AceDB
+function o:OnInitialize()
+  C_Timer.After(1, function() p('xx OnInitialize...') end)
+end
+
+function o:OnEnable()
+  C_Timer.After(1, function() p('xx OnEnable...') end)
+end
