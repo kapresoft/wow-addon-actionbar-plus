0a3b4a2645ee8eaf83fc8d476f55836952b09eeb
Tony Lagnada <deloyz@gmail.com>
2026-02-10 16:06:49 -0800

    Core Init and OnEnable

--- ActionbarPlus-BarsUI/Modules/Tmpx/BarController.lua
+++ ActionbarPlus-BarsUI/Modules/Tmpx/BarController.lua
@@ -22,19 +22,22 @@ local function ControllerMethods()
     local c = ActionbarPlus_BarsUI_BarControllerMixin
 
     function c:OnLoad()
+        self:RegisterEvent('PLAYER_LOGIN')
         self:RegisterEvent('PLAYER_ENTERING_WORLD')
         self:RegisterEvent('UNIT_SPELLCAST_SENT')
     end
     
     function c:OnEvent(evt, ...)
-        p('OnEvent:: event=', evt)
+        C_Timer.After(1, function()
+            p('OnEvent:: event=', evt, 'args=', { ... })
+        end)
 
-        local arg1, arg2 = ...;
+        --[[local arg1, arg2 = ...;
         if ( evt == 'PLAYER_ENTERING_WORLD' ) then
         elseif ( evt == 'UNIT_SPELLCAST_SENT' ) then
             --p('OnEvent:: event=', evt)
             --ActionBarController_UpdateAll();
-        end
+        end]]
     end
     
 end; ControllerMethods()
