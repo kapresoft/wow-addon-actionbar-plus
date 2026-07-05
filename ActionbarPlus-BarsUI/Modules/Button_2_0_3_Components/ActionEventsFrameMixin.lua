a29fbb8893cbf8e43afdebd2864a1c559f812fc2
Tony Lagnada <deloyz@gmail.com>
2026-07-05 13:18:06 -0700

    hide bars when SettingsPanel opens (covers Edit Mode → Actionbar Settings transition)

--- ActionbarPlus-BarsUI/Modules/Button_2_0_3_Components/ActionEventsFrameMixin.lua
+++ ActionbarPlus-BarsUI/Modules/Button_2_0_3_Components/ActionEventsFrameMixin.lua
@@ -53,7 +53,6 @@ ActionEventsFrameMixin_ABP_2_0 = {};
 local o = ActionEventsFrameMixin_ABP_2_0
 
 -- todo: in tbc, drag-start from a bag item no longer fires show grid, but ITEM_LOCKED_CHANGED, ITEM_UNLOCKED (in this order), TBC
--- todo: the cooldown text is too large in extra buttons
 -- todo: Edit Mode -> Action Bar Settings doesn't restore bars
 -- todo: complete localization
 -- todo: masque integration
